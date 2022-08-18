b685ba7f206927b08f9cf9eb3788b06b16c9a4a2
Val <valprado.v1@gmail.com>
2022-08-18 22:47:06 +0300

    Rewil 1u zadachu

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zada4a 2.
+
+/*
+Console.Write("Enter 1st value: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2nd value: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine($"{num1} is bigger then {num2} ");
+}
+else
+{
+    Console.WriteLine($"{num2} is bigger then {num1} ");
+}
+*/
+
+// Zada4a 4.
+
