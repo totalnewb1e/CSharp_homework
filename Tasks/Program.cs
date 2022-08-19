75a9e28599ec0df282b281ea5639ce109c67bc01
Val <valprado.v1@gmail.com>
2022-08-19 15:12:15 +0300

    1 task left

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -18,3 +18,36 @@ else
 
 // Zada4a 4.
 
+/*
+Console.Write("Enter 1st value: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2nd value: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 3rd value: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num2 > max) max = num2;
+if(num3 > max) max = num3;
+
+Console.WriteLine($" maximum number is {max} ");
+*/
+
+// Zada4a 6.
+
+/*
+Console.Write("Enter ur value: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num %2 == 0) // Остаток от деления на "2" - %2, возможно найти отстаток от деления на любое число и получить также любое. " %3 == 3 ".
+{
+    Console.WriteLine($" {num} is even ");
+}
+else
+{
+    Console.WriteLine($" {num} is odd");
+}
+*/
+
+// Zada4a 8.
